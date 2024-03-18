1c186c314c6cbbeaa6540773bda8a185a014da6f
Pawel Klapuch <pawelklapuch@gmail.com>
2024-03-17 19:19:03 -0700

    Added Image / Added string resource

--- Basics/app/src/main/java/com/example/basics/MainActivity.kt
+++ Basics/app/src/main/java/com/example/basics/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,8 +13,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.basics.ui.theme.BasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,13 +39,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
-        Text(
-            text = "Hello:  $name!",
-            modifier = modifier.padding(24.dp)
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    Box(modifier) {
+        Image(
+            painter = painterResource(R.drawable.androidparty),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+
+        GreetingText(
+            message = message,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
         )
     }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Cyan) {
+        GreetingText(message = stringResource(R.string.welcome_title), modifier = modifier)
+//        Text(
+//            text = "Hello:  $name!",
+//            modifier = modifier.padding(24.dp)
+//        )
+    }
+}
+
+@Composable
+fun GreetingText(message: String, modifier: Modifier = Modifier) {
+    Text(
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp,
+    )
 }
 
 @Preview(showBackground = true)
@@ -47,5 +82,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     BasicsTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    BasicsTheme {
+        GreetingImage(
+            message = "Happy Birthday Sam!",
+            from = "From Emma"
+        )
     }
 }
